7d9704ca1c647c34a0f7ee5066f65d83f9433923
Viznem <thinhtruong3344@gmail.com>
2023-10-25 18:00:51 +0700

    auth login system

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { AuthService } from './auth.service';
+import { User } from 'src/user/entities/user.entity';
+import { LoginUserDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+
+    constructor(private authService: AuthService){}
+
+    @Post('register')
+    register(@Body() registerUserDto:RegisterUserDto): Promise<User> {
+        console.log('register api');
+        console.log(registerUserDto);
+        return this.authService.register(registerUserDto);
+    }
+
+    @Post('login')
+    @UsePipes(ValidationPipe)
+    login(@Body() loginUserDto:LoginUserDto):Promise<any> {
+        console.log(loginUserDto)
+
+        return this.authService.login(loginUserDto)
+    }
+
+    @Post('refresh-token')
+    refreshToken(@Body() {refresh_token}):Promise<any> {
+        console.log('refresh token api')
+        return this.authService.refreshToken(refresh_token);
+    }
+
+}
